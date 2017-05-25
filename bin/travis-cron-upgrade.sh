16680d892645949b6254371bd0e3f9164f1bf663
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-26 07:30:38 +0900

    Fix master

--- bin/travis-cron-upgrade.sh
+++ bin/travis-cron-upgrade.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-# if [[ $TRAVIS_EVENT_TYPE != "cron" ]]; then
-#   echo 'Deploy perfomed only on cronjob.'
-#   exit 0
-# fi
-
-
+if [[ $TRAVIS_EVENT_TYPE != "cron" ]]; then
+  echo 'Deploy perfomed only on cronjob.'
+  exit 0
+fi
 yarn upgrade
 
 if [[ "$(git --no-pager diff)" == "" ]]; then
@@ -17,11 +15,9 @@ git config user.name 'kamataryo@travis'
 git config user.email "kamataryo@users.noreply.github.com"
 git remote remove origin
 git remote add origin git@github.com:$TRAVIS_GH_REPO_SLUG.git
+git checkout master
 git add .
 git commit -m "Upgrade package"
-echo '===== test ====='
-git status
-git branch
 
 npm test
 npm version patch -m "Upgrade package"
