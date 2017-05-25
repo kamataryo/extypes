102a9aac11ff966e54b488a174df6ffdfd19cdde
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-25 18:55:41 +0900

    Fix travis

--- bin/travis-pull-request.sh
+++ bin/travis-pull-request.sh
@@ -17,12 +17,6 @@ if [[ $(git diff) == "" ]]; then
 else
   PATCH_NAME=patch__package-upgrade-$(date '+%s')
 
-  # install GitHub hub
-  HUB_URL="https://github.com/github/hub/releases/download/v2.2.9/hub-linux-arm64-2.2.9.tgz"
-  curl -L $HUB_URL > "$HOME/hub.tgz"
-  tar zxvf "$HOME/hub.tgz"
-  export PATH="$PATH:$HOME/hub-linux-amd64-$HUB"
-
   git config user.name 'kamataryo@travis'
   git config user.email "kamataryo@users.noreply.github.com"
   git checkout -b $PATCH_NAME
@@ -30,9 +24,5 @@ else
   git commit -m "Upgrade package[ci skip]"
   git remote add upstream git@github.com:$TRAVIS_GH_REPO_SLUG.git
   git push upstream $PATCH_NAME
-  ls $HOME/hub-linux-arm64-2.2.9/bin/hub
-  ls $HOME/hub-linux-arm64-2.2.9/bin
-  ls $HOME/hub-linux-arm64-2.2.9
-  ls $HOME
-  $HOME/hub-linux-arm64-2.2.9/bin/hub pull-request -m "Upgrade package"
+  hub pull-request -m "Upgrade package"
 fi
