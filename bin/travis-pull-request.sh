4de153e60877d36fc0f0e53223834e1c6d4f5549
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-25 18:06:18 +0900

    Fix travis

--- bin/travis-pull-request.sh
+++ bin/travis-pull-request.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 
 if [[ $CI != "true" && $TRAVIS != "true" ]]; then
   echo 'Not available witout travis CI.'
@@ -16,20 +15,20 @@ yarn upgrade
 if [[ $(git diff) == "" ]]; then
   exit 0
 else
-  PATCH_NAME=patch-package-upgrade-$(date '+%s')
+  PATCH_NAME=patch__package-upgrade-$(date '+%s')
 
   # install GitHub hub
   HUB_VERSION=2.2.9
-  HUB_URL="https://github.com/github/hub/releases/download/v"$HUB_VERSION"/hub-linux-arm64-"$HUB_VERSION".tgz"
+  HUB_URL="https://github.com/github/hub/releases/download/v2.2.9/hub-linux-arm64-2.2.9.tgz"
   curl $HUB_URL > "$HOME/hub.tgz"
   tar zxvf "$HOME/hub.tgz"
 
   git config user.name 'kamataryo@travis'
   git config user.email "kamataryo@users.noreply.github.com"
-  git remote add origin "git@github.com:$TRAVIS_REPO_SLUG.git"
   git checkout -b $PATCH_NAME
   git add .
   git commit -m "Upgrade package"
-  git push origin $PATCH_NAME
+  git remote add upstream git@github.com:$TRAVIS_GH_REPO_SLUG.git
+  git push upstream $PATCH_NAME
   $HOME/hub-linux-arm64-2.2.9/bin/hub pull-request -m "Upgrade package"
 fi
