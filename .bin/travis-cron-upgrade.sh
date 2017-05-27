cec4bcf8a14641136f729e22def6907819a0438f
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-05-27 10:23:34 +0900

    Fix build

--- .bin/travis-cron-upgrade.sh
+++ .bin/travis-cron-upgrade.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ $TRAVIS_EVENT_TYPE != "cron" || $TRAVIS_BRANCH == "master" ]]; then
+BRANCH_TO_DEPLOY=__travis-branch-to-deploy
+
+if [[ $TRAVIS_EVENT_TYPE != "cron" || $TRAVIS_BRANCH != "master" ]]; then
   echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
   echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-  echo 'Deploy perfomed only on cronjob and with master.'
+  echo 'Deploy perfomed only on cronjob and with `master` branch.'
   exit 0
 fi
 
@@ -18,10 +20,13 @@ fi
 git config user.name 'kamataryo@travis'
 git config user.email "kamataryo@users.noreply.github.com"
 git remote remove origin
-git remote add origin git@github.com:$TRAVIS_GH_REPO_SLUG.git
+git remote add origin git@github.com:$GH_REF.git
+
+git branch $BRANCH_TO_DEPLOY
+
 git checkout master
 git add .
 git commit -m "Upgrade package [cron]"
-
-npm test
 npm version patch -m "Upgrade package [cron]"
+
+git checkout $BRANCH_TO_DEPLOY
