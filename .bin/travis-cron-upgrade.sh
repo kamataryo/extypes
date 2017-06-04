e07395787607d1b3caa652d3341c2c083313768f
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-06-05 08:39:42 +0900

    Fix build

--- .bin/travis-cron-upgrade.sh
+++ .bin/travis-cron-upgrade.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ $TRAVIS_TAG == "" ]]; then
+  echo 'Auto-upgrade will not be perfomed against tagged commit.'
+  exit 0
+fi
+
 BRANCH_TO_DEPLOY="__travis-branch-to-deploy"
 
 if [ $TRAVIS_EVENT_TYPE != "cron" ] || [ $TRAVIS_BRANCH != "master" ]; then
   echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
   echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-  echo 'Auto-upgrade perfomed only on cronjob and with `master` branch.'
+  echo 'Auto-upgrade will be perfomed only on cronjob and with `master` branch.'
   exit 0
 fi
 
